8eff0d58b434715dc57cf307569138f233289955
CudraMohamed <ncudramohamed@gmail.com>
2022-02-16 10:53:07 +0300

    second file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+        val name: String = "Mercy Makaya"
+        println(name)
+        val age: String = "20yrs"
+        println(age)
+        val phoneNumber: String = "0789987809"
+        println(phoneNumber)
+        val weight: Double = 45.2
+        println(weight)
+        val citizenship = "ugandan"
+        println(citizenship)
+}
